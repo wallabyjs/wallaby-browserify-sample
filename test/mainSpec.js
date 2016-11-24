55e5975e700142c7ab9d7deee1641cef1c48dde6
clif edwards <clif.edwards.2@gmail.com>
2016-11-24 09:45:23 -0500

    A STABLE WORKING wallaby TESTING main.js USING jasmine TO ACCESS index.html

--- test/mainSpec.js
+++ test/mainSpec.js
@@ -1,7 +1,7 @@
-// "use strict";
-
 var chai = require('chai');
-var expect = chai.expect;
+// var expect = chai.expect;
+var should = chai.should();
+var _ = require('ramda');
 
 function isNodeList(nodes) {
     var stringRepr = Object.prototype.toString.call(nodes);
@@ -12,22 +12,30 @@ function isNodeList(nodes) {
         (nodes.length === 0 || (typeof nodes[0] === "object" && nodes[0].nodeType > 0));
 }
 
-
-var chpts = document.querySelectorAll(".chptr, span");
-console.log("chpts:" + chpts);
-
 describe('chpts', function () {
+    var chpts;
+    beforeEach(function () {
+        loadFixtures('index.html');
+        chpts = document.querySelectorAll(".chptr span");
+    });
     it('should exist', function () {
-        expect(chpts).to.exist;
+        chpts.should.exist;
     });
-
     it('should be node list', function () {
-        expect(isNodeList(chpts)).true;
+        isNodeList(chpts).should.be.true;
     });
-    it('should have children. WHY DOES IT FAIL?? ', function () {
-        expect(chpts.length).is.equal(5);
+    it('should have children.', function () {
+        chpts.length.should.equal(5);
     });
-
+    it('should have a firstChild.', function () {
+        chpts[0].should.exist;
+    });
+    it('should have abbreviated head.innerHTML.', function () {
+        var TST = _.compose(
+            _.slice(1, 8),
+            _.prop('innerHTML'),
+            _.head
+        );
+        TST(chpts).should.equal("  1 And");
+    });
 });
-
-
