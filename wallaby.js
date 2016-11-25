1fd85f27fb85814a6a697de8e2037aded197f33a
clif edwards <clif.edwards.2@gmail.com>
2016-11-25 11:42:04 -0500

    SUCCESS MUTATED the test span Elt.style.fontSize. MAYBE it is time to add bable to the wallaby config file. Amoung more important things that will rid me of all the "do not use var - use let..." A STABLE WORKING wallaby TESTING main.js USING jasmine TO ACCESS index.html

--- wallaby.js
+++ wallaby.js
@@ -1,8 +1,18 @@
 var wallabify = require('wallabify');
-var wallabyPostprocessor = wallabify({});
+var wallabyPostprocessor = wallabify({
+        // browserify options, such as
+        // insertGlobals: false
+    }
+    // you may also pass an initializer function to chain other
+    // browserify options, such as transformers
+    // , b => b.exclude('mkdirp').transform(require('babelify'))
+);
 
 module.exports = function () {
     return {
+        // set `load: false` to all of the browserified source files and tests,
+        // as they should not be loaded in browser,
+        // their browserified versions will be loaded instead
         files: [
             {pattern: 'node_modules/jquery/dist/jquery.js', instrument: false},
             {pattern: 'node_modules/jasmine-jquery/lib/jasmine-jquery.js', instrument: false},
