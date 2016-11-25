0a7cbd16531e571a0cfdfd80048caef118c2f133
clif edwards <clif.edwards.2@gmail.com>
2016-11-25 08:25:00 -0500

    beginning to use mocha before() facilitated by using describles within a higher level describe. In other words I am learning Mocha. A STABLE WORKING wallaby TESTING main.js USING jasmine TO ACCESS index.html

--- test/mainSpec.js
+++ test/mainSpec.js
@@ -1,62 +1,67 @@
+"use strict";
+
 var chai = require('chai');
+var before = chai.before;
 var should = chai.should();
 var _ = require('ramda');
 //---------------------------
 var isNodeList = require('../src/isNodeList');
-// var chpts;
-// chai.before(function () {
-//     loadFixtures('index.html');
-//     chpts = document.querySelectorAll(".chptr span");
-// });
-describe('chpts', function () {
-    var chpts;
+
+var chpts;
+describe(" index.html.", function () {
+    var chpts, span, _fontSizeLens;
     beforeEach(function () {
         loadFixtures('index.html');
         chpts = document.querySelectorAll(".chptr span");
-    });
-    it('should exist', function () {
-        chpts.should.exist;
-    });
-    it('should be node list', function () {
-        isNodeList(chpts).should.be.true;
-    });
-    it('should have some children.', function () {
-        chpts.length.should.be.greaterThan(0);
-    });
-    it('should have a firstChild.', function () {
-        chpts[0].should.exist;
-    });
-    it('should have some innerHTML.', function () {
-        var someHTML = _.compose(
-            _.slice(1, 8),
-            _.prop('innerHTML'),
-            _.head
-        );
-        someHTML(chpts).should.equal("  1 And");
-    });
-});
-describe("a Span", function () {
-    var span;
-    beforeEach(function () {
-        loadFixtures('index.html');
-        span = document.querySelectorAll(".chptr span")[0];
-    });
-    it("should exist.", function () {
-        span.should.exist;
-    });
-    it("should have empty style.fontSize", function () {
-        span.style.fontSize.should.be.equal('');
-    });
-});
-describe("fontSize::span.style.fontSize", function () {
-    var span, _fontSizeLens;
-    beforeEach(function () {
+        span = chpts[0];
         _fontSizeLens = _.lensPath(['style', 'fontSize']);
-        loadFixtures('index.html');
-        span = document.querySelectorAll(".chptr span")[0];
 
     });
-    it("should exist.", function () {
-        _.view(_fontSizeLens, span).should.exist;
+    describe('chpts', function () {
+        it('should exist', function () {
+            chpts.should.exist;
+        });
+        it('should be node list', function () {
+            isNodeList(chpts).should.be.true;
+        });
+        it('should have some children.', function () {
+            chpts.length.should.be.greaterThan(0);
+        });
+        it('should have a firstChild.', function () {
+            chpts[0].should.exist;
+        });
+        it('should have some innerHTML.', function () {
+            var someHTML = _.compose(
+                _.slice(1, 8),
+                _.prop('innerHTML'),
+                _.head
+            );
+            someHTML(chpts).should.equal("  1 And");
+        });
+    });
+    describe("a Span", function () {
+        it('should have some innerHTML.', function () {
+            var someHTML = _.compose(
+                _.slice(1, 8),
+                _.prop('innerHTML'),
+                _.head
+            );
+            someHTML(chpts).should.equal("  1 And");
+        });
+        it("should have empty style.fontSize", function () {
+            span.style.fontSize.should.be.equal('');
+        });
+    });
+    describe("fontSize::span.style.fontSize", function () {
+        var span, _fontSizeLens;
+        beforeEach(function () {
+            _fontSizeLens = _.lensPath(['style', 'fontSize']);
+            loadFixtures('index.html');
+            span = document.querySelectorAll(".chptr span")[0];
+
+        });
+        it("should exist.", function () {
+            _.view(_fontSizeLens, span).should.exist;
+        });
     });
 });
