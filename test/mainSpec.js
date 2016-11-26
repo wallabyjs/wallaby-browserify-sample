6719c94d9eea2882356ed708b2a6468a5336771f
clif edwards <clif.edwards.2@gmail.com>
2016-11-26 11:32:54 -0500

    it(should be mutated using _setStylProp(S,N,D). -> WIP STABLE

--- test/mainSpec.js
+++ test/mainSpec.js
@@ -12,21 +12,21 @@ var _slctSpan_byNdx = curry(function (span_ndx) {
     return _.nth(span_ndx)
 });// (N) -> L -> D
 var _stylePropName = _.prop('style');
-var _setStylePropLens = _.lensProp;// S -> Lens
-var _aStylDct = _.prop('style');
-var _viewStyleProp = curry(function (prop_str, a_dct) {
-    return _.view(_setStylePropLens(prop_str), _aStylDct(a_dct))
+var _setStylPropLens = _.lensProp;// S -> Lens
+var _aStylDct = _.prop('style'); // D -> D.style
+var _viewStylProp = curry(function (prop_str, a_dct) {
+    return _.view(_setStylPropLens(prop_str), _aStylDct(a_dct))
 }); // (S -> Lens) -> D -> S
 
-// var _setStylePropValue = function (ndx) {
+// var _setStylPropValue = function (ndx) {
 //     return ndx * 0.1
 // }; // N:ndx -> N:wt
-var _setStyleProp = curry(function (prop_str, val_str, a_dct) {
-    return _.set(_setStylePropLens(prop_str), val_str, _aStylDct(a_dct))
+var _setStylProp = curry(function (prop_str, val_str, a_dct) {
+    return _.set(_setStylPropLens(prop_str), val_str, _aStylDct(a_dct))
 });// S _> N -> D
 // var _setStyle = curry(
 //     function (styl_prop_name, span_ndx, chptSpns) {
-//         return _setStyleProp(styl_prop_name);
+//         return _setStylProp(styl_prop_name);
 //     });
 
 // TESTS ----------------------------
@@ -87,9 +87,9 @@ describe("mutating Styles/", function () {
                 fontSize.should.equal((''));
             });
             it("should be mutated.", function () {
-                var new_spanStyle = _.set(_fontSizeLens, "50%", spanStyle);
-                console.log("new_spanStyle:" + new_spanStyle.fontSize);
-                new_spanStyle.fontSize.should.equal(('50%'));
+                var new_spanStyl = _.set(_fontSizeLens, "50%", spanStyle);
+                console.log("new_spanStyl:" + new_spanStyl.fontSize);
+                new_spanStyl.fontSize.should.equal(('50%'));
             });
         });
     });
@@ -114,23 +114,27 @@ describe("mutating Styles/", function () {
             });
         });
         describe("view || set a Span.style using lens/", function () {
-            var aSpan, lens, styleProp;
+            var aSpan, lens, styleProp, spanStyl;
             beforeEach(function resetFontSize() {
                 chptSpns = document.querySelectorAll(".chptr span");
                 aSpan = _slctSpan_byNdx(0)(chptSpns);
-                lens = _setStylePropLens('fontSize');
-                styleProp = _viewStyleProp('fontSize', aSpan)
+                lens = _setStylPropLens('fontSize');
+                spanStyl = _aStylDct(aSpan);
+                styleProp = _viewStylProp('fontSize', aSpan)
             });
-            it("build a lens using _setStylePropLens(_stylePropName('fontSize'))", function () {
+            it("build a lens using _setStylPropLens(_stylePropName('fontSize'))", function () {
                 lens.should.exist;
             });
-            it("span prop fontSize should be empty: using _viewStyleProp(S,D)", function () {
+            it("span style dict should have a fontSize attribute: using _aStylDct(D)", function () {
+                _aStylDct(aSpan).fontSize.should.exist;
+            });
+            it("span style fontSize should be empty: using _viewStylProp(S,D)", function () {
                 styleProp.should.equal('');
             });
-            it("should be mutated using _setStyleProp(S,N,D).", function () {
-                var new_spanStyle = _setStyleProp(lens, "50%", aSpan);
-                console.log("new_spanStyle:" + new_spanStyle);
-                new_spanStyle.fontSize.should.equal(('50%'));
+            it("should be mutated using _setStylProp(S,N,D).", function () {
+                var new_spanStyl = _setStylProp('fontSize', "50%", aSpan);
+                console.log("new_spanStyl:" + new_spanStyl.fontSize);
+                new_spanStyl.fontSize.should.equal(('50%'));
             });
         });
     });
