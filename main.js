6c9bef5106016be5561ba768644f0aba755a795d
clif edwards <clif.edwards.2@gmail.com>
2016-11-28 17:27:36 -0500

    STABLE  INVOKED main.js FROM bundle IN index.html. USED mutate(prop, valu, elt) AND SAW effect IN inde.html

--- main.js
+++ main.js
@@ -1,22 +1,27 @@
 /**
- *  expect I can call mtt_Elt.mutate from main ans see effect in index.html
+ *  expect I can call src/mtt_Elt.mutate from main ans see effect in index.html
  */
 "use strict";
 
-// var _ = require('ramda');
-// var R = require('ramda');
-// var map = _.map;
-// var compose = _.compose;
-// var curry = _.curry;
-// var P = require('ramda-fantasy');// new
-// var Identity = P.Identity;
-// var Maybe = P.Maybe;
-// var IO = P.IO.IO;
-// var runIO = P.IO.runIO;
-// var assert = require('assert');
+let _ = require('ramda');
+let map = _.map;
+let compose = _.compose;
+let curry = _.curry;
+// let P = require('ramda-fantasy');// new
+// let Identity = P.Identity;
+// let Maybe = P.Maybe;
+// let IO = P.IO.IO;
+// let runIO = P.IO.runIO;
 
 //*********************************************
 let TRK = "main.js:web-browser-Sample", RET;
 console.log("< IN >" + TRK);
+
+let chptSpns = document.querySelectorAll(".chptr span");
+let aSpan = _.nth(2)(chptSpns);
+let mutate = require('./src/mutateElt');
+let elt = mutate('fontSize', "150%")(aSpan);
+
+console.log(aSpan.innerHTML);
 
 console.log('  OUT>' + TRK);
