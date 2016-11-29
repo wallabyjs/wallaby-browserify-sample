532d9c93326802f59e18b0121aeb89e862e4cf3f
clif edwards <clif.edwards.2@gmail.com>
2016-11-29 06:51:59 -0500

    STABLE tests USING mutateCSD() BEFORE ADDING mutateRClss()

--- test/mtt_EltSpec.js
+++ test/mtt_EltSpec.js
@@ -1,5 +1,6 @@
 /**
- *MUTATES a spanElt using module.export:: mutate
+ * describe(" mutate a span parent: readCls/"
+ * describe(" mutateCSD a span styleProp/"
  * STABLE
  */
 
@@ -41,32 +42,36 @@ let _setStylPropLens = _.lensProp;// S -> Lens
 // });
 
 /**
- *  ..... mutate( S:prop_name -> a:prop_valu -> Elt: HTML_elt)  -> Elt
+ *  ..... mutateCSD( S:prop_name -> a:prop_valu -> Elt: HTML_elt)  -> Elt
  *  USE:reset a specific HTML Style Object [CSD]  property and returns the mutated Element
  */
-let mutate;
-mutate = curry((prop_name, prop_valu, elt) => {
-    elt.style[prop_name] = prop_valu;
-    return elt
-});
-//OR
-mutate = require('../src/mutateElt');
+let mutateCSD = require('../src/mutateElt').mutateCSD;
 // TESTS ----------------------------
 describe("mutating Styles/", function () {
+    let chptSpns, aSpan, fontSizeProp;
+    beforeEach(() => {
+        loadFixtures('index.html');
+        chptSpns = document.querySelectorAll(".chptr span");
+        aSpan = _.nth(0)(chptSpns);
+        fontSizeProp = 'fontSize';
+    });
     describe(" mutate a span styleProp/", function () {
-        let chptSpns, aSpan;
-        beforeEach(() => {
-            loadFixtures('index.html');
-            chptSpns = document.querySelectorAll(".chptr span");
-            aSpan = _.nth(0)(chptSpns)
-        });
-        describe("update an Elt.{ CSD object i.e. elt.style}/", function () {
+        describe("update an Elt.CSD object i.e.P elt.style}/", function () {
             it("should update the CSD object fontSize  USING _setEltCsd", function () {
-                let propName = 'fontSize';
                 let newVal = '40%'; // PLAN: this will be val=f(elt ndx relative to its siblings AND which readCls it is)
-                let newSpan = mutate(propName, newVal, aSpan);
+                let newSpan = mutateCSD(fontSizeProp, newVal)(aSpan);
                 newSpan.style.fontSize.should.not.equal('');
                 newSpan.style.fontSize.should.equal(newVal);
+            });
+        });
+    });
+    describe(" mutate a span parent: readCls/", function () {
+        describe("update an Elt.CSD object i.e.P elt.style}/", function () {
+            it("should move the spanElt into a divReadCls USING _setEltCsd", function () {
+                let newVal = '40%'; // PLAN: this will be val=f(elt ndx relative to its siblings AND which readCls it is)
+                // let newSpan = mutate(fontSizeProp, newVal)(aSpan);
+                // newSpan.style.fontSize.should.not.equal('');
+                // newSpan.style.fontSize.should.equal(newVal);
             });
         });
     });
