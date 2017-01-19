24e19efd8adfe446cf638bf12017394b0e6d7c83
clif edwards <clif.edwards.2@gmail.com>
2017-01-19 12:38:43 -0500

    MOVED  a  function: _ElemWTER.js into src/ directory AND TESTED  in test/_ElemWTER.js REMOVED _ElemWTER.js code,  ADDED a require _ElemWTER.js in composeWter_FormatterSpec
    MOVED  a  function: _Elem_ReadClss_WTER.js into src/ directory AND TESTED  in test/_Elem_ReadClss_WTER.js
    REMOVED _Elem_ReadClss_WTER.js code,  ADDED a require _Elem_ReadClss_WTER.js in composeWter_FormatterSpec

--- data/CsdLimits.js
+++ data/CsdLimits.js
@@ -3,7 +3,7 @@
 // let CsdLimits = {
 module.exports = {
     pst: {csdBeg: 0.2, csdEnd: 1.0},
-    cur: {csdBeg:1, csdEnd:1},
+    cur: {csdBeg:1, csdEnd:1.0},
     fut: {csdBeg:1, csdEnd:.4}};
 
 // module.exports =CsdLimits;
--- test/composeWter_FormatterSpec.js
+++ test/composeWter_FormatterSpec.js
@@ -33,6 +33,8 @@ let expect = chai.expect;
 let have = chai.have;
 
 // FUNCTIONS
+let roundToTwoPlaces = require('../h/roundToTwoPlaces');
+
 let _ReadClss_CsdLimits; // STR.readClssName -> DCT.readClss_CsdLimits
 _ReadClss_CsdLimits = require('../src/ReadClss_CsdLimits');
 
@@ -44,8 +46,8 @@ _formatFontSize = require('../src/Elem_Style_Formatters')._formatFontSize;
 
 
 // CODE UNDER TEST
-let _ElemWTER, Elem_WT;// D->L->N -> N
-xdescribe(`_ElemWTER:: D -> L -> N -> N returns this_Elem's relative Weight 
+let _ElemWTER = ()=>{}, Elem_WT;// D->L->N -> N
+describe(`_ElemWTER:: D -> L -> N -> N returns this_Elem's relative Weight 
         asFnOf  Its_ReadClss 
                 to set its Weight Limits
         and     Its_RelativePosition 
@@ -67,6 +69,7 @@ xdescribe(`_ElemWTER:: D -> L -> N -> N returns this_Elem's relative Weight
     _ElemWTER = curry(
         // (dct, lst, ndx) => _delta(dct) / _incLength(lst) * ( 1+ndx) + _beg(dct)
         (dct, lst, ndx) => roundToTwoPlaces(_delta(dct) / _incLength(lst) * _incNdx(ndx) + _beg(dct))); //.8/2*1+.2 ->.6
+    // PstCsdLimits = {csdBeg: 0.2, csdEnd: 1.0}
     // wt = delta / (len+1) * (ndx+1)
     // some pst tests for pst: {csdBeg: 0.2, csdEnd: 1.0}
     // Len: 0    1   2   3   4   5
@@ -78,25 +81,32 @@ xdescribe(`_ElemWTER:: D -> L -> N -> N returns this_Elem's relative Weight
 
     it("should return weights:PstCsdLimits:{csdBeg: 0.2, csdEnd: 1.0}, Lst:[0], ndx:0", function () {
         // ((1 -.2 -> .8)/(0+2->2))-> .4 * (0+1->1) + .2 -> .6
-        _ElemWTER(Pst_CsdLimits, [0],0).should.equal(0.6);// .8/2*1 + .2  ->
+        _ElemWTER({csdBeg: 0.2, csdEnd: 1.0}, [0],0).should.equal(0.6);// .8/2*1 + .2  ->
     });
     it("should return weights[csdBeg, csdEnd] for lst.length:2", function () {
-        _ElemWTER(_CsdLimits().pst)([0, 1])(0).should.equal(0.47);// .8 / 3 * 1 + .2 -> .47
-        _ElemWTER(_CsdLimits().pst)([0, 1])(1).should.equal(0.73); //.8 / 3 * 2 + .2 -> .73
+        _ElemWTER({csdBeg: 0.2, csdEnd: 1.0},[0, 1])(0).should.equal(0.47);// .8 / 3 * 1 + .2 -> .47
+        _ElemWTER({csdBeg: 0.2, csdEnd: 1.0})([0, 1])(1).should.equal(0.73); //.8 / 3 * 2 + .2 -> .73
     });
     it("should return weights[csdBeg, csdEnd] for lst.length:2", function () {
-        _ElemWTER(_CsdLimits().pst)([0, 1])(0).should.equal(0.47);// .8 / 3 * 1 + .2 -> .47
-        _ElemWTER(_CsdLimits().pst)([0,1,2,3,4])(4).should.equal(0.87);
+        _ElemWTER({csdBeg: 0.2, csdEnd: 1.0})([0, 1])(0).should.equal(0.47);// .8 / 3 * 1 + .2 -> .47
+        _ElemWTER({csdBeg: 0.2, csdEnd: 1.0})([0,1,2,3,4])(4).should.equal(0.87);
     });
 });
 
 let _Elem_ReadClss_WTER;// S->L->N -> N
-xdescribe("_Elem_ReadClss_WTER(S,L,N) REFACTED w/ R.useWith from _ElemWTER(D,L,N) ", function () {
-    _Elem_ReadClss_WTER = R.useWith(_ElemWTER, [_ReadClss_CsdLimits, identity, identity]); // Str -> L, N -> N
+_Elem_ReadClss_WTER = R.useWith(_ElemWTER, [_ReadClss_CsdLimits, identity, identity]); // Str -> L, N -> N
+
+describe("_Elem_ReadClss_WTER(S,L,N) REFACTED w/ R.useWith from _ElemWTER(D,L,N) ", function () {
+    it(`CONFIRM data/CsdLimits.pst should deep = { csdBeg: 0.2, csdEnd: 1.0 }`, function () {
+        let CsdLimits = require('../data/CsdLimits');
+        // console.log(CsdLimits.pst);
+        (CsdLimits.pst).should.deep.equal( { csdBeg: 0.2, csdEnd: 1.0 });
+    });
     it("should expect the 1st argument as a String", function () {
-        _Elem_ReadClss_WTER('pst')( ['a', 'b'], 0).should.equal(.47);
+        _Elem_ReadClss_WTER(('pst'),  ['a', 'b'], 0).should.equal(.47);
         _Elem_ReadClss_WTER('pst')( ['a', 'b'], 1).should.equal(.73);
-        _Elem_ReadClss_WTER('fut')( ['a', 'b', 'c'], 0).should.equal(.4);// -.6/2*1+1
+        // ref Fut_ReadClss:     fut: {csdBeg:1, csdEnd:.4}};
+        _Elem_ReadClss_WTER('fut')( ['a', 'b', 'c', ], 0).should.equal(.85);// (.4-1 -> -.6)/(3+1)*(0+1) +1 -> -.6/4 -> -.15 +1
     });
 });
 
