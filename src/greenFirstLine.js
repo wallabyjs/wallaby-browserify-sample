3bdfd42c1b6508e0038011bdeca79bbb19c4e992
clif edwards <clif.edwards.2@gmail.com>
2017-01-29 09:13:03 -0500

    using R.invoker to set styles in the DOM

--- src/greenFirstLine.js
+++ src/greenFirstLine.js
@@ -7,19 +7,27 @@ let R = require('ramda');
 
 // greenFirstLine
 module.exports =  dom => {
-    let elt = dom.querySelector('#theFirst');
     let csd = {fontSize: "45%", opacity: "0.3", color: "green"};
-    let eltStyle = elt.style;
-    eltStyle.opacity = '0.73';
-    eltStyle.color = 'red';
-    eltStyle.color = 'green';
-    // lets try lens
-    let colorLens = R.lensProp('color');
-    R.set(colorLens, 'green', eltStyle);
+    let elt = dom.querySelector('#theFirst');
+    let styleCSD = elt.style;
+    styleCSD.backgroundColor = 'pink';
+    styleCSD.opacity = '0.73';
+    styleCSD.color = 'red';
+    styleCSD.setProperty('color', 'green', '');// this works
+    // // LETS TRY invoker
+    let styleColor = R.invoker(2, 'setProperty')('color');
+    styleColor('green', styleCSD);
+    let styleOpacity = R.invoker(2, 'setProperty')('opacity');
+    styleOpacity('.4', styleCSD);
+    //let // lets try lens
+    // let colorLens = R.lensProp('color');
+    // R.set(colorLens, 'green', styleCSD);
     // lets try assoc
-    // R.assoc('color', 'green', eltStyle);
+    // R.assoc('color', 'green', styleCSD);
     // let sty =  (num, key, obj) =>  obj.key = num  ;
-    // R.mapObjIndexed(sty, csd)(eltStyle);
-    return dom
+    // R.mapObjIndexed(sty, csd)(styleCSD);
+    // LETS TRY
+    // Object.assign(styleCSD, csd);
+    // return dom // SEEMINGLY NOT NEEDED!!
 };
 
