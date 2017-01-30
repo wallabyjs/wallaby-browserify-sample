0bf099954613f46d39b8288af1f1e1907e87812b
clif edwards <clif.edwards.2@gmail.com>
2017-01-30 11:43:41 -0500

    getting close to finalizing setVersionNum and added a data Dictionary VersionDct

---
+++ data/VersionDct.js
@@ -0,0 +1,9 @@
+/**
+ * Created by CLIF on 1/30/2017.
+ */
+"use strict";
+/**
+ * version: -> S
+ * @type {{version: string}}
+ */
+module.exports = {version: '0.0.3'};
--- src/setVersionNumOnIndexHTML.js
+++ src/setVersionNumOnIndexHTML.js
@@ -5,16 +5,46 @@
 "use strict";
 
 let R = require('ramda'),
-    curry = R.curry;
+    curry = R.curry,
+    compose = R.compose;
+// ***************************
+let VersionDct = require('../data/VersionDct');
+
+let cb = x => console.log('   -> ' + x);
+let getVersion = R.prop('version');// DCT -> STR
+let formatVersion = vers_str => "wbSample ver: " + vers_str;// STR -> STR
 /**
- * setVersionNumOnIndexHTML:: DOM -> DOM
+ * ..... getVersionStr:: DCT -> STR
+ */
+let getVersionStr = R.compose(formatVersion, getVersion);
+/**
+ *  ..... getTheTitleElem:: DOC -> Elem
+ */
+let getTheTitleElem = R.invoker(1, 'querySelector')('title');//DOC -> ELEM
+
+/**
+ * ..... mutateTitle_VersionNumber:: DOC -> DOC
+ *      sets document titleElement to
+ *
+ * getVersionStr:: DCT -> STR
+ * getTheTitleElem:: DOC -> Elem
+ * setInnerHTML_value:: Elem -> Elem
  * @param doc
  */
 module.exports = curry(
     doc => {
-        let ver_str = '0.0.3';
-        //TODO: require this from some data/version object
-        let title = doc.querySelector('title');
-        title.innerHTML = `wbSample ver: ${ver_str}`;
+        /**
+         * .... setInnerHTML_value:: Elem -> Elem
+         */
+        let setInnerHTML = el => el.innerHTML = getVersionStr(VersionDct)
+        ;// EL -> EL
+        compose(R.tap(cb), setInnerHTML, getTheTitleElem)(doc);
+
+        /**
+         * ..... mutateTitle:: DOC -> DOC
+         */
+        const mutateTitle_VersionNumber = compose(setInnerHTML, getTheTitleElem); // DOC -> DOC
+        // compose(R.tap(cb), mutateTitle_VersionNumber)(doc);
+
         return doc
     });
