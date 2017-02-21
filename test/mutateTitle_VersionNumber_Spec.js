af905823bfa8f23ab8d1702bdf2e6c4bdbb6688e
clif edwards <clif.edwards.2@gmail.com>
2017-02-21 09:37:04 -0500

    FIXED test

--- test/mutateTitle_VersionNumber_Spec.js
+++ test/mutateTitle_VersionNumber_Spec.js
@@ -18,7 +18,7 @@ let chai = require('chai'),
 let mutateTitle_VersionNumber = require('../src/mutateTitle_VersionNumber');
 const VersionDct = require('../data/VersionDct');
 
-describe(`the fn: mutateTitle_VersionNumber:: Doc -> Doc `, ()=>{
+describe(`the fn: mutateTitle_VersionNumber:: Doc -> Doc `, () => {
     let doc, dct, fn;
     mocha.beforeEach(() => {
         loadFixtures('index.html');
@@ -31,15 +31,15 @@ describe(`the fn: mutateTitle_VersionNumber:: Doc -> Doc `, ()=>{
         (fn).should.be.a('function');
     });
     it(`should return a document object.`, () => {
-        doc.should.be.a('object').equal(doc)        ;
+        doc.should.be.a('object').and.deep.equal(doc);
     });
     it(`should always return a title including 'wbSample ver:`, () => {
-        R.prop('title',doc).should.include('wbSample ver:');
+        R.prop('title', doc).should.include('wbSample ver:');
     });
-    it(`should return a title value '1_2_3' retrieved from modifying ../data/VersionDct'`, ()=>{
+    it(`should return a title value '1_2_3' retrieved from modifying ../data/VersionDct'`, () => {
         dct.version = '1_2_3';
         mutateTitle_VersionNumber(doc);
-        R.prop('title',doc).should.include('1_2_3');
+        R.prop('title', doc).should.include('1_2_3');
     });
 });
 
