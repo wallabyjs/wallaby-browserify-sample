9d28e3cb6b337f45d0360ce9436718b856434a89
clif edwards <clif.edwards.2@gmail.com>
2017-03-07 11:47:55 -0500

    STABLE TESTED Fn00 , FnXX and Fn01 are functions returning a RclssName  given a currentRclssBounds Dict and some element info

--- test/Fn01_Test.js
+++ test/Fn01_Test.js
@@ -13,7 +13,7 @@ let chai = require('chai'),
 
 let Fn00 = curry(
     /**
-     *  ..... Fn00
+     *  ..... Fn00:: DCT->EL->N->LST -> STR
      * @param dct
      * @param el
      * @param ndx
@@ -29,7 +29,20 @@ let Fn00 = curry(
                 'cur'
     });
 
-let FnXX = curry((currBounds, el, el_ndx, el_lst) => Fn00(currBounds)(el, R.__, el_lst));//
+let Fn01 = curry(
+    /**
+     *  ..... Fn01:: ((DCT,EL,__,LST) -> N) -> STR
+     * @param currBounds
+     * @param el
+     * @param el_ndx
+     * @param el_lst
+     *
+     * ACT: Fn returns a CURRIED, PARTIAL FnXX applied with params: currBounds, el, __, el_lst
+     * the returned FnXX returns a RclssName when applied to param:el_ndx
+     * PAS: a partialed FnXX is returned when params:currBounds, el, __, and el_lst are applied.
+     * an RclssName is returned when FnXX is applied to el_ndx
+     */
+    (currBounds, el, el_ndx, el_lst) => Fn00(currBounds)(el, R.__, el_lst));//
 
 let currBounds = {beg: 2, lng: 2};
 let elArg = {el:'', ndx:0, lst:[0, 1, 2, 3, 4]};
@@ -50,7 +63,7 @@ describe(`Fn00 returns the Rclss Identity of an Element of a Set `, () => {
     });
     describe(`Fn01 is a partialed Fn00(currBounds, elArg.el, R.__, elArg.lst) `, () => {
         let Fn01 = Fn00(currBounds)(elArg.el, R.__, elArg.lst);
-        FnXX = Fn00(currBounds)(elArg.el, R.__, elArg.lst);
+        // FnXX = Fn00(currBounds)(elArg.el, R.__, elArg.lst);
         it(` invoked it should return an Rclss name`, () => {
             expect(Fn01(0)).to.equal('pst');
             expect(Fn01(1)).to.equal('pst');
