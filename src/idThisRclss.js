6b3ba1cc4b7683f23e48cae41b9d6584055dba87
clif edwards <clif.edwards.2@gmail.com>
2017-03-07 14:28:59 -0500

    USED both Fn00 and Fn01 versions of idThisRclss().

--- src/idThisRclss.js
+++ src/idThisRclss.js
@@ -30,7 +30,6 @@ let Fn01 = curry(
      *  ..... Fn01:: ((DCT,EL,__,LST) -> N) -> STR
      * @param currBounds
      * @param el
-     * @param el_ndx
      * @param el_lst
      *
      * ACT: Fn returns a CURRIED, PARTIAL FnXX applied with params: currBounds, el, __, el_lst
@@ -38,6 +37,6 @@ let Fn01 = curry(
      * PAS: a partialed FnXX is returned when params:currBounds, el, __, and el_lst are applied.
      * an RclssName is returned when FnXX is applied to el_ndx
      */
-    (currBounds, el, el_ndx, el_lst) => Fn00(currBounds)(el, R.__, el_lst));//
+    (currBounds, el, el_lst) => Fn00(currBounds)(el, R.__, el_lst));//
 
 module.exports ={Fn01, Fn00};
--- test/idThisRclss_Test.js
+++ test/idThisRclss_Test.js
@@ -1,6 +1,6 @@
 
 /**
- *  lets name this module. identityRclss OR IdentityOfRclss OR idRclss OR idThisRclss
+ *  idThisRclss_Test.js
  */
 "use strict";
 
@@ -14,52 +14,61 @@ let chai = require('chai'),
 //
 
 
-let Fn00 = require('../src/idThisRclss').Fn00;
 let thisID = require('../src/idThisRclss').Fn00;
+let thisID1 = require('../src/idThisRclss').Fn01;
 
 let currBounds = {beg: 2, lng: 2};
 let elArg = {el:'', ndx:0, lst:[0, 1, 2, 3, 4]};
 
-describe(`Fn00 returns the Rclss Identity of an Element of a Set `, () => {
-    describe(`Fn00 has params: CurrentRclssBoundsDict && elem params`, () => {
+describe(`thisID returns the Rclss Identity of an Element of a Set `, () => {
+    describe(`thisID has params: CurrentRclssBoundsDict && elem params`, () => {
         it(`should be a function of length 4`, () => {
-            expect(Fn00).to.be.a('function').with.lengthOf(4);
+            expect(thisID).to.be.a('function').with.lengthOf(4);
         });
         it(`partialed it should  to be function of length 3`, () => {
-            expect(Fn00(currBounds)).to.be.a('function').with.lengthOf(3);
+            expect(thisID(currBounds)).to.be.a('function').with.lengthOf(3);
         });
         it(`partialed more it should  to be function of length 1`, () => {
-            expect(Fn00(currBounds)('', R.__, [])).to.be.a('function').with.lengthOf(1);
+            expect(thisID(currBounds)('', R.__, [])).to.be.a('function').with.lengthOf(1);
         });
 
     });
 
-    let thisID = Fn00(currBounds)(elArg.el, R.__, elArg.lst);
-    describe(`thisID is a partial invoked  Fn00(currBounds, elArg.el, R.__, elArg.lst) `, () => {
-        it(` invoked it should return an Rclss name`, () => {
-            expect(thisID(0)).to.equal('pst');
-            expect(thisID(1)).to.equal('pst');
-            expect(thisID(2)).to.equal('cur');
-            expect(thisID(3)).to.equal('cur');
-            expect(thisID(4)).to.equal('fut');
+
+    describe(`thisID is a partial invoked  thisID(currBounds, elArg.el, R.__, elArg.lst) `, () => {
+        let cut;
+        beforeEach(() => {
+            cut = thisID(currBounds)('', R.__, []);
+        });
+        it(` invoked it should return an Rclss name`,
+             () => {
+            expect(cut(0)).to.equal('pst');
+            expect(cut(1)).to.equal('pst');
+            expect(cut(2)).to.equal('cur');
+            expect(cut(3)).to.equal('cur');
+            expect(cut(4)).to.equal('fut');
         });
     });
     describe(`Begin testsd against border and error input `, () => {
+        let cut1;
+        beforeEach(() => {
+            cut1 = thisID1(currBounds)('', []);
+        });
         it(` 
         Test: thisID invoked with elemNdx < elemList.length and > elemList.length; 
         The returned names are valid. 
         BUT are they HARMFULL when passes on to the next Fn? 
         OR will they ever be used?
         The input elemList WILL ALWAYS = 0 OR greater untill it's length`, () => {
-            expect(thisID(-11)).to.equal('pst');
-            expect(thisID(-1)).to.equal('pst');
-            // expect(thisID(0)).to.equal('pst');
-            // expect(thisID(1)).to.equal('pst');
-            // expect(thisID(2)).to.equal('cur');
-            // expect(thisID(3)).to.equal('cur');
-            // expect(thisID(4)).to.equal('fut');
-            expect(thisID(elArg.lst.length)).to.equal('fut', 'Even though list.length as an index is beyond the range of the list.');
-            expect(thisID(10)).to.equal('fut');
+            expect(cut1(-11)).to.equal('pst');
+            expect(cut1(-1)).to.equal('pst');
+            expect(cut1(0)).to.equal('pst');
+            expect(cut1(1)).to.equal('pst');
+            expect(cut1(2)).to.equal('cur');
+            expect(cut1(3)).to.equal('cur');
+            expect(cut1(4)).to.equal('fut');
+            expect(cut1(elArg.lst.length)).to.equal('fut', 'Even though list.length as an index is beyond the range of the list.');
+            expect(cut1(10)).to.equal('fut');
         });
     });
 });
