3bbe27a764c521360fc2d1410207ddfd7694bbd9
clif edwards <clif.edwards.2@gmail.com>
2017-03-11 07:53:22 -0500

    working towards using main.js and index.html for real document testing.

--- main.js
+++ main.js
@@ -5,6 +5,5 @@ let TRK = "wbSample/main.js";
 console.log("< IN >" + TRK);
 // let _mutate = require('./src/mutateTheDOM');
 // _mutate(document);
-// let _test = require('./test/Fn01_Test');
-// _test(document);
 console.log('  OUT>' + TRK);
+let stop = 0;
