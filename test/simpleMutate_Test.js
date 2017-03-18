73770de285d7e240167c871eb79a13270bfb345a
clif edwards <clif.edwards.2@gmail.com>
2017-03-18 08:32:47 -0400

    simpleMutate_Test.js STABLE WIP begin the Fn: _mutate_aVerse TO BE  mapped ONTO _allVerse(document)

--- test/simpleMutate_Test.js
+++ test/simpleMutate_Test.js
@@ -51,72 +51,78 @@ describe(`map(mutate_aVerse)(aSet of HTML document/chapter/Verses)
 
     I want to mutate all of the Verses in a Chapter;
     This is accomplished by map('mutate_aVerse)(document)`, () => {
-    let doc, _mutate_aVerse;
+    let doc;
     mocha.beforeEach(function () {
         loadFixtures('index.html');
         doc = document;
     });
 
-    describe(`first confirm the Document
+    describe(`FIRST - the Data Fn: _allVerses 
+     this is a verbose series of tests: from begin to final Fn..`, () => {
+        describe(`first confirm the Document
     `, () => {
-        it(`should not be 'null'`, () => {
-            expect(doc).to.exist.and.not.to.be.a('null');
+            it(`should not be 'null'`, () => {
+                expect(doc).to.exist.and.not.to.be.a('null');
+            });
         });
-    });
-    describe(`confirm Fn:_aChpt(doc) HAS an element 'chptr'
+        describe(`confirm Fn:_aChpt(doc) HAS an element 'chptr'
     `, () => {
-        it(`should.be an div.chptr element.`, () => {
-            expect(_aChpt(doc)).to.exist.and.not.to.be.a('null');
+            it(`should.be an div.chptr element.`, () => {
+                expect(_aChpt(doc)).to.exist.and.not.to.be.a('null');
+            });
+            it(`should have an element: 'chptr`, () => {
+                expect(_aChpt(doc).className)
+                    .to.be.equal('chptr');
+            });
         });
-        it(`should have an element: 'chptr`, () => {
-            expect(_aChpt(doc).className)
-                .to.be.equal('chptr');
-        });
-    });
-    describe(`_aChpt(doc)should HAVE children'
+        describe(`_aChpt(doc)should HAVE children'
         `, () => {
-        it(`should have at least one child`, () => {
-            expect(_aChpt(doc).childElementCount)
-                .to.be.gt(1);
-        });
-        it(`should have a SPAN first Child`, () => {
-            expect(_aChpt(doc).children[0].tagName)
-                .eql('SPAN');
-        });
+            it(`should have at least one child`, () => {
+                expect(_aChpt(doc).childElementCount)
+                    .to.be.gt(1);
+            });
+            it(`should have a SPAN first Child`, () => {
+                expect(_aChpt(doc).children[0].tagName)
+                    .eql('SPAN');
+            });
 
-    });
-    describe(`confirm the Fn: _children(_aChpt(doc)) RETURNS all the Verses [Spans] 
+        });
+        describe(`confirm the Fn: _children(_aChpt(doc)) RETURNS all the Verses [Spans] 
     `, () => {
-        it(`should arrayLike`, () => {
-            expect(R.isArrayLike(_children(_aChpt(doc)))).isT;
+            it(`should arrayLike`, () => {
+                expect(R.isArrayLike(_children(_aChpt(doc)))).isT;
+            });
+            it(`should have at least 0ne child`, () => {
+                expect(_children(_aChpt(doc)).length).to.be.gt(0);
+            });
+            it(`should have a Span first Child`, () => {
+                expect(_children(_aChpt(doc))[0].tagName)
+                    .eql('SPAN');
+            });
         });
-        it(`should have at least 0ne child`, () => {
-            expect(_children(_aChpt(doc)).length).to.be.gt(0);
-        });
-        it(`should have a Span first Child`, () => {
-            expect(_children(_aChpt(doc))[0].tagName)
-                .eql('SPAN');
-        });
-    });
-    describe(`now  confirm the Fn:_allVerse(doc) RETURNS all the Verses [Spans].
+        describe(`now  confirm the Fn:_allVerse(doc) RETURNS all the Verses [Spans].
     
         This is the final setOf_Verses to map the Fn: _mutate_aVerse on to.`, () => {
-        it(`should be arrayLike`, () => {
-            expect(R.isArrayLike(_allVerses(doc))).ok;
+            it(`should be arrayLike`, () => {
+                expect(R.isArrayLike(_allVerses(doc))).ok;
+            });
+            it(`should have at least 0ne child`, () => {
+                expect(_allVerses(doc).length).to.be.gt(0);
+            });
+            it(`should have a Span first Child`, () => {
+                expect((_allVerses(doc))[0].tagName)
+                    .equal('SPAN');//   NOTE: double quotes:(_allVerses(doc))
+            });
         });
-        it(`should have at least 0ne child`, () => {
-            expect(_allVerses(doc).length).to.be.gt(0);
-        });
-        it(`should have a Span first Child`, () => {
-            expect((_allVerses(doc))[0].tagName)
-                .equal('SPAN');//   NOTE: double quotes:(_allVerses(doc))
-        });
-    });
-    describe(`NOW switch to Fn:_mutate_aVerse`, () => {
-        it(`should not be 'null'`, () => {
-            expect(_mutate_aVerse).to.exist;
-        });
-    });
 
+    });
+    describe(`SECOND: the map Fn: _mutate_aVerse`, () => {
+        describe(`Fn:_mutate_aVerse`, () => {
+            it(`should exis'`, () => {
+                expect(_mutate_aVerse).to.be.a('function');
+            });
+        });
+
+    });
 });
 
