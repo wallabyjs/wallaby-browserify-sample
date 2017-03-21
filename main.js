c7561c2ca5b2c9627303d3a2f6c51fb32a0e8672
clif edwards <clif.edwards.2@gmail.com>
2017-03-21 10:57:41 -0400

    main.js    CONFIRMED with a hardcoded visual test that ./src/mutate_anElemStyleAttr() WORKS. I am trying to find a way to debug details in a mocha test AS WELL AS the overall not debuggable wallaby tests.

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 "use strict";
+let h = require('./h/H');
 
 //******************** main *************************
 let TRK = "wbSample/main.js";
 console.log("< IN >" + TRK);
-// let _mutate = require('./src/mutateTheDOM');
-// _mutate(document);
-console.log('  OUT>' + TRK);
+
+let _mutate = require('./src/mutate_anElemStyleAttr');
+
+let _aSpan = doc => doc.querySelectorAll('div .chptr, span')[0];// -> NL
+let aSpan = _aSpan(document);
+
+
+console.log(' -> ' + aSpan.style.color);
+
+_mutate("opacity:0.4; color:red", aSpan);
+
+console.log(' -> ' + aSpan.style.color);
+
+console.log(' OUT> ' + TRK);
 let stop = 0;
