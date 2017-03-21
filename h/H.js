ad27ec5c53acf94f74624eeea228352d391dbff7
clif edwards <clif.edwards.2@gmail.com>
2017-03-21 07:14:55 -0400

    h/H ADDED  _thisSet_MUTATED_by) AND _thisSet_MUTATED_by()

--- h/H.js
+++ h/H.js
@@ -21,3 +21,34 @@ const Spans_FROM_theDocument_GIVEN_aSelector = curry(
     (q_str, doc) => doc.querySelectorAll(q_str)
 );
 module.exports.Spans_FROM_theDocument_GIVEN_aSelector = Spans_FROM_theDocument_GIVEN_aSelector;
+
+/**
+ *  two point of views for ending up with all Verses mutated.
+ *  (1)thisSet_MUTATED_by ::  FIRST -> Set (HAS set,  WANTS Fn )  -> Fn  ->  TO RET Set
+ *  (2)_APPLY_thisFn_to ::       FIRST -> Fn  (HAS Fn ,  WANTS Set)  -> Set ->  TO RET Set
+ *
+ */
+
+const _thisSet_MUTATED_by = curry(
+    /**
+     *  .....   thisSet_MUTATED_by:: SET -> Fn -> SET
+     * FIRST -> aSet (now HAS set,  WANTS Fn)  -> Fn  ->  TO RET aSet
+     * @param set
+     * @param fn
+     */
+    (set, fn) => R.addindex(R.map)(fn, set));
+
+const _APPLY_thisFn_to = curry(
+    /**
+     *  .... _APPLY_thisFn_to:  Fn -> Set -> Set
+     *  (now HAS Fn, WANTS set)
+     * @param fn
+     * @param set
+     */
+    (fn, set) => R.addindex(R.map)(fn, set));
+
+module.exports = {
+    _thisSet_MUTATED_by,
+    _APPLY_thisFn_to,
+    Spans_FROM_theDocument_GIVEN_aSelector
+};
