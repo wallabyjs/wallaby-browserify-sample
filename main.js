d86e0cb54dc4350614bfe63150850bbebc0b3ebf
clif edwards <clif.edwards.2@gmail.com>
2017-03-24 17:02:35 -0400

    [main.js]     WIP BROKEN , NO TESTS: CAN NOT WORK AFTER A SPLIT CHANGE

--- main.js
+++ main.js
@@ -9,32 +9,7 @@ let h = require('./h/H');
 let TRK = "wbSample/main.js";
 console.log("< IN >" + TRK);
 
-// ************* FUNCTIONS
-let mutate_anAttr = require('./h/alter/anElemStyleAttr');
-let Redify_anAtttr = mutate_anAttr("color:red");
-let redify_anElem = curry(
-    /**
-     *
-     * @param el
-     * @param ndx
-     * @param set
-     * @return : a modified Elem
-     */
-    (el, ndx, set) => Redify_anAtttr(el)
-);
-
-// ************* DATA
-let select_aFewVerses = R.slice(0, 2); // SET -> SET
-let ret_allChprVerses = doc => doc.querySelectorAll('div .chptr, span'); // (doc) -> SET
-let aSet = pipe(ret_allChprVerses, select_aFewVerses); // SET -> SET
-
-// ************* MAIN: CodeUnderTest:: MAP_aSet(FN)(DATA)
-let MAP_aSet = require('./h/alter/aSet'); // (Fn -> SET) -> SET
-
-const RED_ify_aSetOfVerses = doc => {
-    MAP_aSet(redify_anElem, aSet(doc));
-};
-// ************* INVOKE this Fn as the main
-RED_ify_aSetOfVerses(document);
+let mut = require('./src/RED_ify_someChptVerses');
+mut(document);
 
 console.log(' OUT> ' + TRK);
